cf39f571355ee84cd06b5090987c0bb663f877e5
Hibiki1020 <hibikijitaku@gmail.com>
2022-11-18 12:30:37 +0900

    s\helper.py修正

--- scripts/models/helpers.py
+++ scripts/models/helpers.py
@@ -154,12 +154,18 @@ def load_pretrained(model, cfg=None, num_classes=1000, in_chans=3, filter_fn=Non
         state_dict[classifier_name + '.weight'] = classifier_weight[1:]
         classifier_bias = state_dict[classifier_name + '.bias']
         state_dict[classifier_name + '.bias'] = classifier_bias[1:]
-    elif num_classes != state_dict[classifier_name + '.weight'].size(0):
+    else:
         #print('Removing the last fully connected layer due to dimensions mismatch ('+str(num_classes)+ ' != '+str(state_dict[classifier_name + '.weight'].size(0))+').', flush=True)
         # completely discard fully connected for all other differences between pretrained and created model
-        del state_dict[classifier_name + '.weight']
-        del state_dict[classifier_name + '.bias']
+        # del state_dict[classifier_name + '.weight']
+        # del state_dict[classifier_name + '.bias']
         strict = False
+    # elif num_classes != state_dict[classifier_name + '.weight'].size(0):
+    #     #print('Removing the last fully connected layer due to dimensions mismatch ('+str(num_classes)+ ' != '+str(state_dict[classifier_name + '.weight'].size(0))+').', flush=True)
+    #     # completely discard fully connected for all other differences between pretrained and created model
+    #     del state_dict[classifier_name + '.weight']
+    #     del state_dict[classifier_name + '.bias']
+    #     strict = False
 
 
     ## Resizing the positional embeddings in case they don't match
