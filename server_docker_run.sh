cf68ace37c5b69c5e06a474a5d043b0a4d9c13f4
Hibiki1020 <hibikijitaku@gmail.com>
2023-01-31 17:22:53 +0900

    Train Param Changed for IROS2023

--- server_docker_run.sh
+++ server_docker_run.sh
@@ -7,7 +7,7 @@ docker run -it \
     --net="host" \
     --gpus all \
     --privileged \
-    --shm-size=490g \
+    --shm-size=470g \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --name="integrated_attitude_estimator" \
